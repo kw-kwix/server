a48911f1b7863479d2c56930a4342770c9f3cd74
yoonlight <yoonlight12@gmail.com>
2022-05-26 19:22:38 +0900

    feat: add recommend model loader

---
+++ model.py
@@ -0,0 +1,33 @@
+from keras.models import load_model, Model
+import numpy as np
+
+
+def scalered_height(height):
+    return (height-170)/30
+
+
+def scalered_wight(weight):
+   return (weight-70)/30
+
+
+def scalered_sex(sex):
+   return sex/2
+
+
+def scalered_bmi(bmi):
+   return (bmi-30)/30
+
+
+def scalered_proficiency(proficiency):
+   return (proficiency-170)/30
+
+
+def input(height, weight, sex, bmi, proficiency):
+    return np.array([height, weight, sex, bmi, proficiency])
+
+
+def predict(x):
+
+    model: Model = load_model("chest_model.h5")
+    y = model.predict(x)
+    return y
