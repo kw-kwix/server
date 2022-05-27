2aa47e668ed5d6974beeb963d39ff899278fb238
yoonlight <yoonlight12@gmail.com>
2022-05-27 21:23:16 +0900

    input의 response를 JSON형태로 변경

--- flask_mdb.py
+++ flask_mdb.py
@@ -78,35 +78,20 @@ def logout():
 
 @app.route("/input", methods=['POST', 'GET'])  # 유저 정보를 받음
 def input():
-    userid = session.get('email', None)
-    if request.method == 'GET':
-        if userid is None:
-            return redirect('/login')
-        else:
-            return Response(jsonify({"status": 200}), 200)
     if request.method == 'POST':
-        userInfo = {}
-        userInfo['age'] = request.form.get('ageInput')
-        userInfo['sex'] = request.form.get('inlineRadioOptions')
-        userInfo['height'] = request.form.get('heightInput')
-        userInfo['weight'] = request.form.get('weightInput')
-        userInfo['exercise_level'] = request.form.get('exercise_level')
-        j = 0
-        for i in range(3):
-            k = i+1
-            st = 'scales' + str(k)
-            if request.form.get(st) == 'on':
-                if i == 0:
-                    j = j + 1
-                else:
-                    j = j + 2 * i
-        userInfo['part'] = j  # 2진수로 받음
-        if not(userInfo['age'] and userInfo['height'] and userInfo['weight'] and userInfo['exercise_level']):
-            return Response(jsonify({"status": 403}), 403)
+        user = {}
+        userInfo = request.get_json()
+        if not(userInfo['age'] and userInfo['height'] and userInfo['weight']):
+            return jsonify(message="정보를 모두 입력하세요."), 403
         else:
-            user = list(KWIX.loginInfo.find({'Email': userid}))
-            KWIX.userInfo.update_one({'id': user[0]['id']}, {'$set': userInfo})
-            return redirect('/recommend')
+            print(userInfo)
+            user['age'] = userInfo['age']
+            user['height'] = userInfo['height']
+            user['weight'] = userInfo['weight']
+            id = KWIX.loginInfo.find_one({'email': userInfo["email"]})["id"]
+            print(KWIX.loginInfo.find_one({'email': userInfo["email"]}))
+            KWIX.userInfo.update_one({'id': id}, {'$set': user})
+            return jsonify(message="success"), 200
 
 
 @app.route('/recommend')  # recommend.html과 연결
