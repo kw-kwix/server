312a94b9fbdbf9294c465dd87b3b6e06cadad20c
yoonlight <yoonlight12@gmail.com>
2022-05-28 21:29:07 +0900

    refactor: add boilerplate code

--- model.py
+++ model.py
@@ -36,17 +36,19 @@ def predict(x):
    y= np.around(y.flatten(),3)
    return y
 
-id = input("id:")
-user_input = db.get_user(id)[0]
-print(user_input)
+if __name__=="__main__":
 
-exercise_list = np.array([[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]])
+   id = input("id:")
+   user_input = db.get_user(id)[0]
+   print(user_input)
 
-user_input[1] = scalered_height(user_input[1])
-user_input[2] = scalered_weight(user_input[2])
-user_input[3] = scalered_sex(user_input[3])
-user_input[4] = scalered_age(user_input[4])
-user_input[5] = scalered_bmi(user_input[5])
-user_input[6] = scalered_proficiency(user_input[6])
+   exercise_list = np.array([[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]])
 
-print(predict([np.array([input_data(user_input[1],user_input[2],user_input[3],user_input[4],user_input[5],user_input[6])]),exercise_list]))
+   user_input[1] = scalered_height(user_input[1])
+   user_input[2] = scalered_weight(user_input[2])
+   user_input[3] = scalered_sex(user_input[3])
+   user_input[4] = scalered_age(user_input[4])
+   user_input[5] = scalered_bmi(user_input[5])
+   user_input[6] = scalered_proficiency(user_input[6])
+
+   print(predict([np.array([input_data(user_input[1],user_input[2],user_input[3],user_input[4],user_input[5],user_input[6])]),exercise_list]))
