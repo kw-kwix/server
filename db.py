9c7f1000d7743a08de360f602465b103c92adac3
yoonlight <yoonlight12@gmail.com>
2022-05-28 21:54:49 +0900

    refactor: separate pandas and mongo function

--- db.py
+++ db.py
@@ -1,25 +1,9 @@
-from config import MONGO_URL
-from pymongo.mongo_client import MongoClient
 import pandas as pd
 
 # pandas to mongodb
-def pd_to_dic(df):
+def pd_to_dic(df: pd.DataFrame):
     return df.to_dict('records')
 
 # mongodb to pandas
 def dic_to_pd(dic):
     return pd.DataFrame.from_records(dic)
-
-def get_user(userid):
-    client = MongoClient(MONGO_URL)
-    userInfo=client.KWIX.userInfo
-    rows=userInfo.find({'id' : userid})
-    data=[]
-    for row in rows:
-        data.append(row)
-
-    pd=dic_to_pd(data)
-
-    data = pd[['id','height','weight', 'sex','age','bmi','during']].to_numpy()
-
-    return data
--- mongo.py
+++ mongo.py
@@ -1,4 +1,5 @@
 from pymongo.database import Database
+from db import dic_to_pd
 
 
 def create_login_info(db: Database, login_info):
@@ -19,3 +20,18 @@ def find_login_info(db: Database, email):
 
 def find_user_id(db: Database, user_id):
     return db.loginInfo.find_one({'id': user_id})
+
+
+def get_user(db: Database, user_id):
+    userInfo = db.userInfo
+    rows = userInfo.find({'id': user_id})
+    data = []
+    for row in rows:
+        data.append(row)
+
+    pd = dic_to_pd(data)
+
+    data = pd[['id', 'height', 'weight', 'sex',
+               'age', 'bmi', 'during']].to_numpy()
+
+    return data
